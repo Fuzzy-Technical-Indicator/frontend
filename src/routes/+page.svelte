864964b6bf518683ddd90b76db122a290f2c85e8
Tanat <ttangun1@gmail.com>
2023-04-08 00:28:26 +0700

    feat: add bollinger band and make chart size matched

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,70 @@
 <script lang="ts">
-	import { CandlestickSeries, Chart, LineSeries, PriceScale, TimeScale } from 'svelte-lightweight-charts';
+	import { CandlestickSeries, Chart, LineSeries, TimeScale } from 'svelte-lightweight-charts';
 	import type { PageData } from './$types';
-	import type { IChartApi, LogicalRange, Range } from 'lightweight-charts';
+	import type { IChartApi, LogicalRange, PriceFormatterFn } from 'lightweight-charts';
 
 	export let data: PageData;
-	let { ohlc, rsi } = data;
+	let { ohlc, rsi, sma, bb_lower, bb_upper } = data;
 
-	let chart: IChartApi | null;
-	let axis: IChartApi | null;
+	let main: IChartApi | null;
+	let rsi_chart: IChartApi | null;
 
-	const handleChartLogicalRangeChange = (
-		e: CustomEvent<LogicalRange | null> & { type: 'visibleLogicalRangeChange' }
+	const handleLogicalRangeChange = (
+		e: CustomEvent<LogicalRange | null> & { type: 'visibleLogicalRangeChange' },
+		api: IChartApi | null
 	) => {
 		const range = e.detail;
-		if (axis && range) axis.timeScale().setVisibleLogicalRange(range);
+		if (api && range) {
+			api.timeScale().setVisibleLogicalRange(range);
+		}
 	};
 
-	const handleAxisLogicalRangeChange = (
-		e: CustomEvent<LogicalRange | null> & { type: 'visibleLogicalRangeChange' }
-	) => {
-		const range = e.detail;
-		if (chart && range) chart.timeScale().setVisibleLogicalRange(range);
+	const priceFn: PriceFormatterFn = (price: number) => {
+		return `${price.toFixed(2).padEnd(10)}`;
 	};
+
+	let c_sty = '';
+
+	const handleResize = () => { // could be more generic
+		const w1 = main ? main.priceScale('right').width() : 0;
+		const w2 = rsi_chart ? rsi_chart.priceScale('right').width() : 0;
+		const margin = Math.max(w1, w2) - Math.min(w1, w2);
+		c_sty = `width: calc(100% - ${margin}px)`;
+	};
+
 </script>
 
 <div class="w-screen h-screen">
-	<Chart ref={(ref) => (chart = ref)} container={{ class: 'w-10/12 h-5/6' }} autoSize={true}>
-		<TimeScale visible={true} on:visibleLogicalRangeChange={handleChartLogicalRangeChange} />
+	<Chart
+		ref={(ref) => (main = ref)}
+		container={{ class: 'h-5/6', style: '' }}
+		autoSize={true}
+		localization={{ priceFormatter: priceFn }}
+	>
+		<TimeScale
+			visible={true}
+			on:visibleLogicalRangeChange={(e) => {
+				handleLogicalRangeChange(e, rsi_chart);
+				handleResize(); // TODO: this is a hack
+			}}
+		/>
 		<CandlestickSeries data={ohlc} />
+		<LineSeries lineWidth={1} data={sma} />
+		<LineSeries lineWidth={1} color={"blue"} data={bb_lower} />
+		<LineSeries lineWidth={1} color={"blue"}  data={bb_upper} />
 	</Chart>
-	<Chart ref={(ref) => (axis = ref)} container={{ class: 'w-10/12 h-1/6' }} autoSize={true}>
-		<TimeScale on:visibleLogicalRangeChange={handleAxisLogicalRangeChange} />
-		<LineSeries data={rsi} />
+	<Chart
+		ref={(ref) => (rsi_chart = ref)}
+		container={{ class: 'h-1/6', style: c_sty }}
+		autoSize={true}
+		localization={{ priceFormatter: priceFn }}
+	>
+		<TimeScale
+			on:visibleLogicalRangeChange={(e) => {
+				handleLogicalRangeChange(e, main);
+				handleResize(); // TODO: this is a hack
+			}}
+		/>
+		<LineSeries lineWidth={2} data={rsi} />
 	</Chart>
 </div>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -21,7 +21,12 @@ interface RsiValue {
 	value: number;
 }
 
-async function fetch_ohlc(resp: Response): Promise<CandlestickData[]> {
+interface DTValue<T> {
+	time: MongoTime;
+	value: T;
+}
+
+async function ohlc_from(resp: Response): Promise<CandlestickData[]> {
 	const ohlc = (await resp.json()) as Ohlc[];
 	const data: CandlestickData[] = ohlc.map((x) => {
 		const t = x.time.$date.$numberLong;
@@ -36,7 +41,7 @@ async function fetch_ohlc(resp: Response): Promise<CandlestickData[]> {
 	return data;
 }
 
-async function fetch_rsi(resp: Response): Promise<LineData[]> {
+async function rsi_from(resp: Response): Promise<LineData[]> {
 	const json = (await resp.json()) as RsiValue[];
 	const data: LineData[] = json.map((x) => {
 		const t = x.time.$date.$numberLong;
@@ -48,20 +53,43 @@ async function fetch_rsi(resp: Response): Promise<LineData[]> {
 	return data;
 }
 
+function bb_idx(data: DTValue<[number, number, number]>[], index: number): LineData[] {
+	return data.map((x) => {
+		return {
+			time: (x.time.$date.$numberLong / 1000) as UTCTimestamp,
+			value: x.value[index]
+		} as LineData;
+	});
+}
+
+async function bb_from(resp: Response): Promise<[LineData[], LineData[], LineData[]]> {
+	const json = (await resp.json()) as DTValue<[number, number, number]>[];
+	return [bb_idx(json, 0), bb_idx(json, 1), bb_idx(json, 2)];
+}
+
 export const load: PageLoad = async ({ fetch }) => {
 	// this is hardcoded for now
-	const ohlc = await fetch_ohlc(
+	const ohlc = await ohlc_from(
 		await fetch(`http://127.0.0.1:8000/api/ohlc?symbol=BTC/USDT&interval=1d`)
 	);
-	const rsi = await fetch_rsi(
-		await fetch(`http://127.0.0.1:8000/api/indicator?symbol=BTC/USDT&interval=1d&kind=rsi`)
+	const rsi = await rsi_from(
+		await fetch(`http://127.0.0.1:8000/api/indicator/rsi?symbol=BTC/USDT&interval=1d`)
+	);
+	const [sma, lower, upper] = await bb_from(
+		await fetch(`http://127.0.0.1:8000/api/indicator/bb?symbol=BTC/USDT&interval=1d`)
 	);
 
 	const filler = ohlc.filter((x) => x.time < rsi[0].time).map((x) => {return {time: x.time, value: NaN } as LineData});
-	rsi.unshift(...filler)
+	rsi.unshift(...filler);
+	sma.unshift(...filler);
+	lower.unshift(...filler);
+	upper.unshift(...filler);
 
 	return {
 		ohlc: ohlc,
-		rsi: rsi
+		rsi: rsi,
+		sma: sma,
+		bb_lower: lower,
+		bb_upper: upper
 	};
 };
