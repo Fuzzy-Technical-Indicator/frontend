0a257df6bc26dbb808c8f76e2f9e6a21f3541f9b
Tanat <ttangun1@gmail.com>
2023-11-22 22:38:56 +0700

    feat(fuz-62): add an ability to add new linguistic var

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -12,14 +12,34 @@
 		Colors
 	} from 'chart.js';
 	import LinguisticVar from '$lib/components/LinguisticVar.svelte';
-	import { invalidateAll } from '$app/navigation';
-	import { createQuery } from '@tanstack/svelte-query';
-	import { api } from '$lib/apiClient';
+	import { createMutation, createQuery, useQueryClient } from '@tanstack/svelte-query';
+	import { api, type UpdateLinguisticVariable } from '$lib/apiClient';
 
 	const settings = createQuery({
 		queryKey: ['settings'],
 		queryFn: () => api().getSettings()
 	});
+
+	const linguisticVarOptions = ['rsi', 'bb'];
+	let currLinguisticVarOpt = linguisticVarOptions[0];
+
+	const client = useQueryClient();
+	const updateMutation = createMutation({
+		mutationFn: (data: UpdateLinguisticVariable) => api().updateSettings(data),
+		onSuccess: () => client.invalidateQueries({ queryKey: ['settings'] })
+	});
+
+	const handleAddLinguisticVar = () => {
+		if (
+			$settings.isSuccess &&
+			!Object.keys($settings.data.linguisticVariables).includes(currLinguisticVarOpt)
+		) {
+			const data = {
+				[currLinguisticVarOpt]: { lowerBoundary: 0, upperBoundary: 0, shapes: {} }
+			};
+			$updateMutation.mutate(data);
+		}
+	};
 
 	ChartJS.register(
 		Title,
@@ -49,13 +69,19 @@
 					}}
 					options={{ responsive: true }}
 				/>
-				<LinguisticVar
-					{info}
-					on:update={() => {
-						invalidateAll();
-					}}
-				/>
+				<LinguisticVar {info} />
 			{/each}
 		{/if}
+
+		<div class="mt-5">
+			<button class="border border-black" on:click={handleAddLinguisticVar}
+				>Add new Linguistic Variable</button
+			>
+			<select bind:value={currLinguisticVarOpt}>
+				{#each linguisticVarOptions as opt}
+					<option value={opt}>{opt}</option>
+				{/each}
+			</select>
+		</div>
 	</div>
 </div>
