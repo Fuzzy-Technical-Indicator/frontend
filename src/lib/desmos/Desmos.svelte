59f44c9ba16267c7c8ac9da75a955e69cb87deb6
Thanawat <76035626+0736b@users.noreply.github.com>
2023-12-07 03:32:21 +0700

    update(fuz-64): navbar
    * feat: navbar chart, settings
    * fix: Desmos is not define

--- src/lib/desmos/Desmos.svelte
+++ src/lib/desmos/Desmos.svelte
@@ -13,8 +13,24 @@
 
 	const colors = ['#c74440', '#2d70b3', '#388c46', '#6042a6', '#fa7e19', '#000000'];
 
+	const loadDesmos = () => {
+		// FIX: SPA when change to settings page, Desmos not load
+		return new Promise((resolve, reject) => {
+			const script = document.createElement('script');
+			script.src =
+				'https://www.desmos.com/api/v1.8/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6';
+			script.id = 'desmos-script';
+			script.async = true;
+			script.onload = resolve;
+			script.onerror = reject;
+			document.head.appendChild(script);
+		});
+	};
+
 	let g: any;
-	afterUpdate(() => {
+	afterUpdate(async () => {
+		await loadDesmos();
+
 		if (g) {
 			g.destroy();
 		}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,13 @@
 	import { QueryClientProvider } from '@tanstack/svelte-query';
 	import '../app.css';
 	import type { PageData } from './$types';
+	import Navbar from '$lib/Navbar.svelte';
 
 	export let data: PageData;
 </script>
 
 <QueryClientProvider client={data.queryClient}>
+	<Navbar />
 	<main>
 		<slot />
 	</main>
