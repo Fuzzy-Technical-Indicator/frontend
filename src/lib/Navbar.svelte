01edbd620a3e2ab4600cf8af0f119a1203e3b689
Thanawat <76035626+0736b@users.noreply.github.com>
2024-03-10 22:06:36 +0700

    update: mobile navbar toggle icon

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -7,7 +7,7 @@
 	};
 	const closeNavbar = () => {
 		showMenu = false;
-	}
+	};
 </script>
 
 <div class="bg-black text-[#D4D4D4]">
@@ -34,20 +34,35 @@
 						type="button"
 						class="text-white hover:text-white focus:outline-none focus:text-white"
 					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="w-6 h-6"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-							/>
-						</svg>
+						{#if !showMenu}
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="w-6 h-6"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+								/>
+							</svg>
+						{:else}
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="w-6 h-6"
+								fill="currentColor"
+								stroke="currentColor"
+								stroke-width="1.5"
+								viewBox="0 0 50 50"
+							>
+								<path
+									d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"
+								/>
+							</svg>
+						{/if}
 					</button>
 				</div>
 			</div>
